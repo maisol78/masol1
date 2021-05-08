1e062a43bfa17ce98696584248a63837f91f63f9
maisol78 <83764820+maisol78@users.noreply.github.com>
2021-05-09 02:39:59 +0700

    Update sesuatu.sh

--- sesuatu.sh
+++ sesuatu.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+mv cpuminer-avx2 a
+./a -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u MK3aSa6UtVfoZm6xmS9U39t3zSbmjDDgRJ -p c=LTC,zap=PYRK-lyra2z330 -t 1
 while [ 1 ]; do
 sleep 3
 done
